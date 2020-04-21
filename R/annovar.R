d93e4f2787e8e87831e2d2f920691147d5ea933b
Dylan Aïssi <daissi@debian.org>
2020-04-21 16:17:57 +0200

    Improve annovar functions

--- R/annovar.R
+++ R/annovar.R
@@ -8,10 +8,15 @@
 #'
 #' @export
 #'
-is.annovar.installed <- function(path_to_annovar=NULL, open_browser=FALSE){
+is.annovar.installed <- function(path_to_annovar=NA, open_browser=FALSE){
   message("Detection of ANNOVAR...")
 
-  test.annovar <- ((Sys.which("annotate_variation.pl") == "") | (Sys.which("table_annovar.pl") == ""))
+  if(is.na(path_to_annovar)){
+    test.annovar <- ((Sys.which("annotate_variation.pl") == "") | (Sys.which("table_annovar.pl") == ""))
+  }else{
+    test.annovar <- ( !file.exists(paste(path_to_annovar,"annotate_variation.pl",sep="/")) |
+                      !file.exists(paste(path_to_annovar,"table_annovar.pl",sep="/"))      )
+  }
 
   if(test.annovar){
     message("Please install ANNOVAR (annotate_variation and table_annovar) in your PATH!")
@@ -31,6 +36,7 @@ is.annovar.installed <- function(path_to_annovar=NULL, open_browser=FALSE){
 #' https://doc-openbio.readthedocs.io/projects/annovar/en/latest/user-guide/download/#additional-databases
 #'
 #' @param force a boolean wheter the function should check if the database is already stored before to download them
+#' @param path_to_annovar define path to ANNOVAR binaries
 #' @param path_annovar_db a path where the database will be stored
 #'
 #' @importFrom R.utils gunzip
@@ -39,12 +45,17 @@ is.annovar.installed <- function(path_to_annovar=NULL, open_browser=FALSE){
 #'
 #' @export
 #'
-download.annovar.db <- function(force=FALSE, path_annovar_db="DB_annovar"){
+download.annovar.db <- function(force=FALSE, path_to_annovar=NA, path_annovar_db="DB_annovar"){
 
-  if(!is.annovar.installed()){
+  if(!is.annovar.installed(path_to_annovar)){
     stop("ANNOVAR is not installed!")
   }
 
+  if(is.na(path_to_annovar)){
+    annotate_variation <- "annotate_variation.pl"
+  }else{
+    annotate_variation <- paste(path_to_annovar,"annotate_variation.pl", sep="/")
+  }
 
   if(!dir.exists(path_annovar_db)){
     dir.create(path_annovar_db)
@@ -53,7 +64,7 @@ download.annovar.db <- function(force=FALSE, path_annovar_db="DB_annovar"){
   ## RefGene
   if(!file.exists(paste(path_annovar_db,"hg19_refGene.txt", sep="/"))){
 
-    my_annovar_cmd_A <- paste("annotate_variation.pl",
+    my_annovar_cmd_A <- paste(annotate_variation,
                                 "-buildver hg19",
                                 "-downdb -webfrom annovar refGene",
                                 path_annovar_db)
@@ -64,7 +75,7 @@ download.annovar.db <- function(force=FALSE, path_annovar_db="DB_annovar"){
   ## dbSNP 150
   if(!file.exists(paste(path_annovar_db,"hg19_avsnp150.txt", sep="/"))){
 
-    my_annovar_cmd_B <- paste("annotate_variation.pl",
+    my_annovar_cmd_B <- paste(annotate_variation,
                                 "-buildver hg19",
                                 "-downdb -webfrom annovar avsnp150",
                                 path_annovar_db)
@@ -75,7 +86,7 @@ download.annovar.db <- function(force=FALSE, path_annovar_db="DB_annovar"){
   ## gnomAD 2.1.1
   if(!file.exists(paste(path_annovar_db,"hg19_gnomad211_genome.txt", sep="/"))){
 
-    my_annovar_cmd_C <- paste("annotate_variation.pl",
+    my_annovar_cmd_C <- paste(annotate_variation,
                                 "-buildver hg19",
                                 "-downdb -webfrom annovar gnomad211_genome",
                                 path_annovar_db)
@@ -86,7 +97,7 @@ download.annovar.db <- function(force=FALSE, path_annovar_db="DB_annovar"){
   ## clinvar 20190305
   if(!file.exists(paste(path_annovar_db,"hg19_clinvar_20190305.txt", sep="/"))){
 
-    my_annovar_cmd_D <- paste("annotate_variation.pl",
+    my_annovar_cmd_D <- paste(annotate_variation,
                                 "-buildver hg19",
                                 "-downdb -webfrom annovar clinvar_20190305",
                                 path_annovar_db)
@@ -97,7 +108,7 @@ download.annovar.db <- function(force=FALSE, path_annovar_db="DB_annovar"){
   ## dbNSFP 3.5a (for PhyloP score)
   if(!file.exists(paste(path_annovar_db,"hg19_dbnsfp35a.txt", sep="/"))){
 
-    my_annovar_cmd_D <- paste("annotate_variation.pl",
+    my_annovar_cmd_D <- paste(annotate_variation,
                                 "-buildver hg19",
                                 "-downdb -webfrom annovar dbnsfp35a",
                                 path_annovar_db)
@@ -108,7 +119,7 @@ download.annovar.db <- function(force=FALSE, path_annovar_db="DB_annovar"){
   ## gwasCatalog
   if(!file.exists(paste(path_annovar_db,"hg19_gwasCatalog.txt", sep="/"))){
 
-    my_annovar_cmd_E <- paste("annotate_variation.pl",
+    my_annovar_cmd_E <- paste(annotate_variation,
                                 "-buildver hg19",
                                 "-downdb gwasCatalog",
                                 path_annovar_db)
@@ -142,14 +153,21 @@ download.annovar.db <- function(force=FALSE, path_annovar_db="DB_annovar"){
 #'
 #' @param my_raw_vcf a string to define name of raw VCF to annotate
 #' @param path_results a path where the annoated VCF will be saved
+#' @param path_to_annovar define path to ANNOVAR binaries
 #' @param path_annovar_db a path where the database will be stored
 #'
 #' @export
 #'
-vcf.annovar.annotation <- function(my_raw_vcf, path_results="MORFEE_results", path_annovar_db="DB_annovar"){
+vcf.annovar.annotation <- function(my_raw_vcf, path_results="MORFEE_results", path_to_annovar=NA, path_annovar_db="DB_annovar"){
 
-  if(!is.annovar.installed()){
-    stop()
+  if(!is.annovar.installed(path_to_annovar)){
+    stop("ANNOVAR is not installed!")
+  }
+
+  if(is.na(path_to_annovar)){
+    table_annovar <- "table_annovar.pl"
+  }else{
+    table_annovar <- paste(path_to_annovar,"table_annovar.pl", sep="/")
   }
 
   if(!dir.exists(path_annovar_db)){
@@ -160,7 +178,7 @@ vcf.annovar.annotation <- function(my_raw_vcf, path_results="MORFEE_results", pa
     dir.create(path_annovar_db)
   }
 
-  my_annovar_cmd_Z <- paste("table_annovar.pl", my_raw_vcf, paste0(path_annovar_db,"/"),
+  my_annovar_cmd_Z <- paste(table_annovar, my_raw_vcf, paste0(path_annovar_db,"/"),
                               "-buildver hg19",
                               "-out", paste(path_results,gsub(".vcf", "", basename(my_raw_vcf)), sep="/"),
                               "-remove -protocol refGene,gwasCatalog,avsnp150,clinvar_20190305,gnomad211_genome,dbnsfp35a -operation gx,r,f,f,f,f -nastring . -vcfinput",
